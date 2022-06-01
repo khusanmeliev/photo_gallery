a03d9eb2d693e8332d8a47884e6d3292536dddc3
khasanmeliev <khasanmeliev2@gmail.com>
2022-06-01 13:31:49 +0500

    photo gallery

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { photos } from "../mock/images";
+import CloseIcon from "@mui/icons-material/Close";
+import "./gallery.css";
+
+function Gallery() {
+  const [model, setModel] = useState(false);
+  const [tempimgSrc, setTempImgSrc] = useState("");
+
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
+
+  return (
+    <>
+      <div className={model ? "model open" : "model"}>
+        <img src={tempimgSrc} />
+        <CloseIcon onClick={() => setModel(false)} />
+      </div>
+      <div className="gallery">
+        {photos.map((item, index) => {
+          return (
+            <div
+              className="pics"
+              key={index}
+              onClick={() => getImg(item.imgSrc)}
+            >
+              <img src={item.imgSrc} style={{ width: "100%" }} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Gallery;
---
+++ src/mock/images.js
@@ -0,0 +1,122 @@
+export const photos = [
+  {
+    id: 1,
+    imgSrc:
+      "https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg",
+  },
+  {
+    id: 2,
+    imgSrc:
+      "https://wallpaperboat.com/wp-content/uploads/2019/05/beautiful-sunset-3.jpg",
+  },
+  {
+    id: 3,
+    imgSrc:
+      "https://mymodernmet.com/wp/wp-content/uploads/2019/12/sinhue-boksberger-forest-photo.jpg",
+  },
+  {
+    id: 4,
+    imgSrc:
+      "https://i.pinimg.com/736x/4f/52/47/4f5247ad9be3c9af991b63be0fb20b34.jpg",
+  },
+  {
+    id: 5,
+    imgSrc:
+      "https://images.unsplash.com/photo-1603783033071-551d35a9a29f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbHBhcGVyJTIwaGR8ZW58MHx8MHx8&w=1000&q=80",
+  },
+  {
+    id: 6,
+    imgSrc:
+      "https://image.winudf.com/v2/image/Y29tLmJlc3RoZC5waWFvbGlhbmdfc2NyZWVuc2hvdHNfM19hMzYyZGQ0ZQ/screen-3.jpg?fakeurl=1&type=.webp",
+  },
+  {
+    id: 7,
+    imgSrc:
+      "https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  },
+  {
+    id: 8,
+    imgSrc:
+      "https://images.unsplash.com/photo-1590418606746-018840f9cd0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Ymx1ZSUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+  },
+  {
+    id: 9,
+    imgSrc: "https://openai.com/content/images/2021/01/2x-no-mark-1.jpg",
+  },
+  {
+    id: 10,
+    imgSrc:
+      "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg",
+  },
+  {
+    id: 11,
+    imgSrc:
+      "https://media.istockphoto.com/photos/sofia-in-orange-picture-id615112296?k=20&m=615112296&s=612x612&w=0&h=DWVy3eyz-0dxddUO5xMVRWSgukA0t2AB2wLnq40Soqw=",
+  },
+  {
+    id: 12,
+    imgSrc:
+      "https://www.ucl.ac.uk/news/sites/news/files/styles/large_image/public/blackhole_website.jpg?itok=hSfwPWsP",
+  },
+  {
+    id: 13,
+    imgSrc:
+      "https://www.gettyimages.dk/gi-resources/images/500px/983794168.jpg",
+  },
+  {
+    id: 14,
+    imgSrc:
+      "https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
+  },
+  {
+    id: 15,
+    imgSrc:
+      "https://i.pinimg.com/originals/a5/11/70/a511707377b4332b85a8d90fbcfb20fe.jpg",
+  },
+  {
+    id: 16,
+    imgSrc:
+      "https://i.pinimg.com/736x/45/5d/64/455d643a6d2bfb508be8a3acb66fa3df.jpg",
+  },
+  {
+    id: 17,
+    imgSrc:
+      "https://c4.wallpaperflare.com/wallpaper/94/663/683/pictures-of-beautiful-creative-design-forest-winter-planet-space-wallpaper-thumb.jpg",
+  },
+  {
+    id: 18,
+    imgSrc:
+      "https://i.pinimg.com/originals/e5/0b/f7/e50bf7afefd65ca24f9206a0e362812e.jpg",
+  },
+  {
+    id: 19,
+    imgSrc:
+      "https://www.whatspaper.com/wp-content/uploads/2021/07/hd-beautiful-wallpaper-whatspaper-12.jpg",
+  },
+  {
+    id: 20,
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+  },
+  {
+    id: 21,
+    imgSrc: "https://wallpaperaccess.com/full/7092222.jpg",
+  },
+  {
+    id: 22,
+    imgSrc: "https://wallpaperaccess.com/full/632198.jpg",
+  },
+  {
+    id: 23,
+    imgSrc: "https://cutewallpaper.org/23/beautiful-wallpaper-hd/679726003.jpg",
+  },
+  {
+    id: 24,
+    imgSrc: "https://wallpaper.dog/large/987433.jpg",
+  },
+  {
+    id: 25,
+    imgSrc:
+      "https://beautifulcoolwallpapers.files.wordpress.com/2011/08/hdmobilewallpaper-1.jpg",
+  },
+];
